4a575056c11d640887e7067303f93c418fded6eb
EDIKEU yann <edikeu@informatique.univ-paris-diderot.fr>
2023-04-30 22:49:53 +0200

    ajt de la fonction reception_inscription

--- client.c
+++ client.c
@@ -10,6 +10,7 @@
 #include "include/global.h"
 
 char * gestion_pseudo() {
+    printf("Veuillez entrer un pseudo :\n");
     char *ret = malloc(PSEUDO_LEN);
     while(1) {
         //memset(ret, 0, PSEUDO_LEN);
@@ -68,7 +69,6 @@ void inscrire_utilisateur(int sock) {
 
 
 void poster_billet(int sock, const char *id, const char *num_fichier, const char *message_billet) {
-
     char bufrec[256];
     memset(bufrec, 0, sizeof(bufrec));
     int rec = recv(sock, bufrec, sizeof(bufrec) - 1, 0);
@@ -82,16 +82,44 @@ void poster_billet(int sock, const char *id, const char *num_fichier, const char
     close(sock);
 }
 
+void reception_inscription(int sock) {
+    // Réception de la réponse du serveur
+    uint16_t response_header[2];
+    uint8_t code_req, num_fil;
+    uint16_t response_id, nb;
+
+    if (recv(sock, response_header, sizeof(response_header), 0) == -1) {
+        perror("Erreur lors de la réception de la réponse du serveur");
+        exit(EXIT_FAILURE);
+    }
+
+    code_req = ntohs(response_header[0] >> 5 & 0x1F);
+    response_id = ntohs(response_header[1] & 0x7FF); // Récupération des 11 bits de poids forts
+    printf("Received message :\nCode de requête -> %hu\nID client(garder l'id) -> %hu\n",code_req,response_id);
+    
+    if (recv(sock, &num_fil, sizeof(num_fil), 0) == -1) {
+        perror("Erreur lors de la réception de la réponse du serveur");
+        exit(EXIT_FAILURE);
+    }
+    if (recv(sock, &nb, sizeof(nb), 0) == -1) {
+        perror("Erreur lors de la réception de la réponse du serveur");
+        exit(EXIT_FAILURE);
+    }
+    printf("Numéro de fil -> %u\nNB -> %u\n", num_fil, nb);
+}
+
+
 int main(int argc, char **argv) {
     // Inscription d'un utilisateur
     int sock_inscription = creer_socket();
     se_connecter_au_serveur(sock_inscription);
     inscrire_utilisateur(sock_inscription);
+    reception_inscription(sock_inscription);
 
-    // Poster un billet
+    /* Poster un billet
     int sock_poster = creer_socket();
     se_connecter_au_serveur(sock_poster);
-    poster_billet(sock_poster, "ID_UTILISATEUR", "NUM_FIL", "MESSAGE_BILLET");
+    poster_billet(sock_poster, "ID_UTILISATEUR", "NUM_FIL", "MESSAGE_BILLET");*/
 
     return 0;
 }
